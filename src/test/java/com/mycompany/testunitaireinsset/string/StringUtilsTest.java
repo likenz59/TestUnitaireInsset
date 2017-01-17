40f82045f6578f843ea1f462b72242f4ea67ef5d
Guilbert Baptiste <guilbert.ba@gmail.com>
2017-01-17 11:35:39 +0100

    commit netbean jenkins

---
+++ src/test/java/com/mycompany/testunitaireinsset/string/StringUtilsTest.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+// */
+/*package com.mycompany.testunitaireinsset.string;
+
+import java.util.Map;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user
+ */
+/*public class StringUtilsTest {
+    
+    public StringUtilsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of isChaineValide method, of class StringUtils.
+     */
+    /*@Test(expected = IllegalArgumentException.class)
+    public void testIsChaineValide() {
+        //given
+        String str = "ag";
+        StringUtils instance = new StringUtils();
+    
+        //when
+        instance.isChaineValide(str);
+        //then
+ 
+    }
+    
+   /* @Test
+    public void testMessagfeExceptionisValide(){
+        String str = "ag";
+        StringUtils instance = new StringUtils();
+        //when
+        try{
+             instance.isChaineValide(str);
+             fail("pas bon");
+        }catch{
+            //then
+            assertThat(ex.getMessage(),is("il faut au moins deux caractère"));
+        }
+    }*/
+
+    /**
+     * Test of inverse method, of class StringUtils.
+     */
+    /*@Test
+    public void testInverse() {
+       //given
+        String str = "toto";
+        StringUtils instance = new StringUtils();
+        String expResult = "otot";
+        //when
+        String result = instance.inverse(str);
+        //then
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of isPalindrome method, of class StringUtils.
+     */
+   /* @Test
+    public void testIsPalindrome() {
+
+        String str = "baptiste";
+        StringUtils instance = new StringUtils();
+        boolean expResult = false;
+        boolean result = instance.isPalindrome(str);
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of compterOccurences method, of class StringUtils.
+     */
+   /*@Test
+    public void testCompterOccurences() {
+
+        String str = "aaa";
+        StringUtils instance = new StringUtils();
+        Map<String, Integer> expResult = null;
+        Map<String, Integer> result = instance.compterOccurences(str);
+        assertEquals(expResult, result);
+
+    }
+    
+}*/
